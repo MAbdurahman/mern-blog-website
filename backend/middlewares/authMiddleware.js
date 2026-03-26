7fde0072822b34f9453fb97e5076cb93d13f7bbe
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-03-26 08:05:30 -0500

    Add functionality for server.js to connect to mongoDB

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+import asyncHandler from '../utils/asyncHandlerUtil.js';
+import messageHandler from '../utils/messageHandlerUtil.js';
+import ErrorHandler from '../utils/errorHandlerUtil.js';
+
+export const authenticateUser = asyncHandler(async (req, res, next) => {
+   const token = req.cookies?.access_token;
+   
+   if (!token) {
+      return next(messageHandler(res, false, 'User must be signed in!', 401));
+   }
+
+   const decodedData = await jwt.verify(token, process.env.JWT_SECRET);
+
+   req.user = await User?.findById(decodedData.id);
+
+   next();
+
+});
+
+export const authorizeRoles = (...roles) => {
+   
+   return (req, res, next) => {
+      if (!roles.includes(req.user.role)) {
+         return next(
+            new ErrorHandler(
+               `Role: ${req.user.role} is not allowed to access this resource`, 403
+            )
+         );
+      }
+      next();
+   }
+}
