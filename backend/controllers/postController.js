c716468b02b699c3b5fc4428271c34d1c86b79c1
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-04-25 18:45:42 -0500

    Add basic structure for controllers/postController.js and routes/postRoutes.js

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,2 +1,58 @@
 
-import PostModel from '../models/postModel.js';
+import Post from '../models/postModel.js';
+import asyncHandler from '../utils/asyncHandlerUtil.js';
+import messageHandler from '../utils/messageHandlerUtil.js';
+
+
+export const getAllPosts = asyncHandler(async (req, res, next) => {
+
+   res.status(200).json({
+      message: 'Successfully retrieved all posts!',
+      success: true,
+   });
+});
+
+export const getSinglePost = asyncHandler(async (req, res, next) => {
+   res.status(200).json({
+      message: 'Successfully retrieved single post!',
+      success: true,
+   });
+});
+
+export const createPostAdmin = asyncHandler(async (req, res, next) => {
+   res.status(200).json({
+      message: 'Admin - successfully created post!',
+      success: true,
+   });
+});
+
+export const getAllPostsAdmin = asyncHandler(async (req, res, next) => {
+
+   res.status(200).json({
+      message: 'Admin - successfully retrieved all posts!',
+      success: true,
+   })
+})
+
+export const getSinglePostAdmin = asyncHandler(async (req, res, next) => {
+
+   res.status(200).json({
+      message: 'Admin - successfully retrieved single post!',
+      success: true,
+   });
+});
+
+export const updatePostAdmin = asyncHandler(async (req, res, next) => {
+
+   res.status(200).json({
+      message: 'Admin - successfully updated post!',
+      success: true,
+   });
+});
+
+export const deletePostAdmin = asyncHandler(async (req, res, next) => {
+   res.status(200).json({
+      message: 'Admin - successfully deleted post!',
+      success: true,
+   });
+});
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,8 +1,21 @@
 /************************** imports **************************/
 import express from 'express';
+import {getAllPosts, getSinglePost, createPostAdmin, getAllPostsAdmin,
+getSinglePostAdmin, updatePostAdmin, deletePostAdmin} from '../controllers/postController.js';
+import {authenticateUser, authorizeRoles} from '../middlewares/authMiddleware.js';
 
 /************************* variables *************************/
 const router = express.Router();
 
+/**************************** routes ****************************/
+router.get('/posts', getAllPosts);
+router.get('/posts/:postId', getSinglePost);
+
+/************************* admin routes *************************/
+router.post('/admin/posts', createPostAdmin);
+router.get('/admin/posts', getAllPostsAdmin);
+router.get('/admin/posts/:postId', getSinglePostAdmin);
+router.put('/admin/posts/:postId', updatePostAdmin);
+router.delete('/admin/posts/:postId', deletePostAdmin);
 
 export default router;
