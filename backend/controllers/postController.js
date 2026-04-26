49313781e48216c3fa5e029f1d4a9b7c9ddc25ad
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-04-26 03:44:57 -0500

    Add some functionality to backend/controllers/postControllers.js

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -5,17 +5,32 @@ import messageHandler from '../utils/messageHandlerUtil.js';
 
 
 export const getAllPosts = asyncHandler(async (req, res, next) => {
+   const posts = await Post.find({});
+   if (!posts.length) {
+      return next(messageHandler(res, false, 'No posts found!', 204));
+   }
 
    res.status(200).json({
       message: 'Successfully retrieved all posts!',
       success: true,
+      posts: posts
    });
 });
 
 export const getSinglePost = asyncHandler(async (req, res, next) => {
+   const {postId} = req.params;
+
+   const post = await Post.findById(postId);
+   if (!post) {
+      return next(messageHandler(res, false, 'Post not found!', 404));
+   }
+
+
    res.status(200).json({
       message: 'Successfully retrieved single post!',
       success: true,
+      post: post
+
    });
 });
 
@@ -27,18 +42,28 @@ export const createPostAdmin = asyncHandler(async (req, res, next) => {
 });
 
 export const getAllPostsAdmin = asyncHandler(async (req, res, next) => {
+   const posts = await Post.find().sort({ title: 1 });
+   const postsCount = posts.length ? posts.length : 0;
 
    res.status(200).json({
       message: 'Admin - successfully retrieved all posts!',
+      postsCount: postsCount,
       success: true,
+      posts: posts
    })
 })
 
 export const getSinglePostAdmin = asyncHandler(async (req, res, next) => {
+   const {postId} = req.params;
+   const post = await Post.findById(postId);
+   if (!post) {
+      return next(messageHandler(res, false, 'Post not found!', 404));
+   }
 
    res.status(200).json({
       message: 'Admin - successfully retrieved single post!',
       success: true,
+      post: post
    });
 });
 
