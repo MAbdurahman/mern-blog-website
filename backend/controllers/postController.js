c406760b304eba94a542050d3bafb9a78f825ae0
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-04-28 04:18:38 -0500

    Update backend/controllers/postController.js

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -2,18 +2,29 @@
 import Post from '../models/postModel.js';
 import asyncHandler from '../utils/asyncHandlerUtil.js';
 import messageHandler from '../utils/messageHandlerUtil.js';
-
+import APIFeatures from '../utils/apiFeaturesUtil.js';
 
 export const getAllPosts = asyncHandler(async (req, res, next) => {
-   const posts = await Post.find({});
-   if (!posts.length) {
-      return next(messageHandler(res, false, 'No posts found!', 204));
-   }
+   const resPerPage = 4;
+   const postsCount = await Post.countDocuments();
+
+   const apiFeatures = new APIFeatures(Post.find(), req.query)
+      .search()
+      .filter()
+
+   let posts = await apiFeatures.query;
+   let filteredPosts = posts.length;
+
+   apiFeatures.pagination(resPerPage);
+   posts = await apiFeatures.query.clone();
 
    res.status(200).json({
-      message: 'Successfully retrieved all posts!',
+      message: 'Posts retrieved successfully!',
       success: true,
-      posts: posts
+      filteredPosts: filteredPosts,
+      postsCount: postsCount,
+      resultsPerPage: resPerPage,
+      posts,
    });
 });
 
@@ -25,7 +36,6 @@ export const getSinglePost = asyncHandler(async (req, res, next) => {
       return next(messageHandler(res, false, 'Post not found!', 404));
    }
 
-
    res.status(200).json({
       message: 'Successfully retrieved single post!',
       success: true,
@@ -35,9 +45,13 @@ export const getSinglePost = asyncHandler(async (req, res, next) => {
 });
 
 export const createPostAdmin = asyncHandler(async (req, res, next) => {
-   res.status(200).json({
-      message: 'Admin - successfully created post!',
+   req.body.author = req.user._id;
+   const post = await Post.create(req.body);
+
+   res.status(201).json({
+      message: 'Admin - product created successfully!',
       success: true,
+      post
    });
 });
 
@@ -69,15 +83,39 @@ export const getSinglePostAdmin = asyncHandler(async (req, res, next) => {
 
 export const updatePostAdmin = asyncHandler(async (req, res, next) => {
 
+   const { postId } = req.params;
+   let post = await Post.findById(postId);
+
+   if (!post) {
+      return next(messageHandler(res, 'Post not found!', 404));
+   }
+
+   post = await Post.findByIdAndUpdate(postId, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+   });
+
    res.status(200).json({
-      message: 'Admin - successfully updated post!',
+      message: 'Admin - post updated successfully!',
       success: true,
+      post,
    });
 });
 
 export const deletePostAdmin = asyncHandler(async (req, res, next) => {
+   const { postId } = req.params;
+   let post = await Post.findById(postId);
+
+   if (!post) {
+      return next(messageHandler(res, 'Post not found!', 404));
+   }
+
+   await Post.deleteOne({_id: post._id});
+
    res.status(200).json({
-      message: 'Admin - successfully deleted post!',
+      message: 'Admin - post deleted successfully!',
       success: true,
+      post: {},
    });
 });
